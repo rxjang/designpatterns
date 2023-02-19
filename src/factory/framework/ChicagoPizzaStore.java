a827671b10d1c6a884c95067740ed6032821ac46
Seohyun Jang <olllehyun@gmail.com>
2023-02-19 21:49:36 +0900

    chap07: adapter pattern

--- src/factory/framework/ChicagoPizzaStore.java
+++ src/factory/framework/ChicagoPizzaStore.java
@@ -2,7 +2,7 @@ package factory.framework;
 
 public class ChicagoPizzaStore extends PizzaStore{
     @Override
-    Pizza createPizza(String type) {
+    public Pizza createPizza(String type) {
         Pizza pizza = null;
         if (type.equals("cheese")) {
             pizza = new ChicagoStyleCheesePizza();
--- src/factory/framework/NYPizzaStore.java
+++ src/factory/framework/NYPizzaStore.java
@@ -1,8 +1,8 @@
 package factory.framework;
 
-public class NYPizzaStore extends PizzaStore{
+public class NYPizzaStore extends PizzaStore {
     @Override
-    Pizza createPizza(String type) {
+    public Pizza createPizza(String type) {
         Pizza pizza = null;
         if (type.equals("cheese")) {
             pizza = new NYStyleCheesePizza();
